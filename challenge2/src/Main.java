a42754490f7a3ca1e1da4aeb66aa1872c50f0a51
David Crossman <22548398+DavidCrossman@users.noreply.github.com>
2022-10-21 18:22:07 +0100

    Source code is now read from source files and a Jar file is now built

--- challenge2/src/Main.java
+++ challenge2/src/Main.java
@@ -2,61 +2,33 @@ import barebones.Lexer;
 import barebones.Parser;
 import barebones.Program;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        String source = """
-                clear X;
-                incr X;
-                incr X;
-                clear Y;
-                incr Y;
-                incr Y;
-                incr Y;
-                clear Z;
-                while X not 0 do;
-                   clear W;
-                   while Y not 0 do;
-                      incr Z;
-                      incr W;
-                      decr Y;
-                   end;
-                   while W not 0 do;
-                      incr Y;
-                      decr W;
-                   end;
-                   decr X;
-                end;""";
+        if (args.length != 1) return;
 
-        String fibSource = """
-                clear first;
-                clear second;
-                incr second;
-                while n do;
-                    clear fib;
-                    decr n;
-                    while fib not first do;
-                        incr fib;
-                    end;
-                    clear first;
-                    while first not second do;
-                        incr fib;
-                        incr first;
-                    end;
-                    while second not fib do;
-                        incr second;
-                    end;
-                end;
-                """;
+        String source;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            source = reader.lines().collect(Collectors.joining("\n"));
+            reader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File \"%s\" not found%n".formatted(args[0]));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         Optional<Program> program = Parser.parse(Lexer.lex(source));
 
         if (program.isPresent()) {
-            HashMap<String, Long> state = new HashMap<>() {{
-                put("n", 20L);
-            }};
+            HashMap<String, Long> state = new HashMap<>();
             program.get().execute(state);
             state.forEach((name, value) -> System.out.printf("%s: %s%n", name, value));
         }
