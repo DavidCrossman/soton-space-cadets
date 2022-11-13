5452f132166b324ef55698080f32786337400d60
David Crossman <davidocrossman@gmail.com>
2022-11-13 01:15:07 +0000

    Drawing the spirograph is now animated

--- challenge5/src/main/java/com/david/spirograph/SpirographController.java
+++ challenge5/src/main/java/com/david/spirograph/SpirographController.java
@@ -1,5 +1,6 @@
 package com.david.spirograph;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -9,29 +10,67 @@ public class SpirographController {
     @FXML
     private Canvas canvas;
 
-    private final int timeStep;
+    private final double timeStep;
+    private long startTime;
+    private double lastUpdateTime;
+    private double timer;
+    private final double speed;
+
+    private boolean firstFrame;
+
+    private final AnimationTimer animation;
 
     public SpirographController() {
-        timeStep = 20;
+        timeStep = 0.0001;
+        lastUpdateTime = 0;
+        startTime = 0;
+        firstFrame = true;
+        timer = 0;
+        speed = 50;
+
+        animation = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (firstFrame) {
+                    startTime = now;
+                    firstFrame = false;
+                }
+                now -= startTime;
+
+                timer += now * .000000001 - lastUpdateTime;
+
+                double t = lastUpdateTime;
+                while (timer > timeStep) {
+                    timer -= timeStep;
+                    t += timeStep;
+
+                    GraphicsContext context = canvas.getGraphicsContext2D();
+                    double centreX = canvas.getWidth() * .5;
+                    double centreY = canvas.getHeight() * .5;
+
+                    double r = 134, R = 81, O = 99;
+                    double x = (R - r) * Math.cos(t * speed) + O * Math.cos(((R - r) / r) * t * speed);
+                    double y = (R - r) * Math.sin(t * speed) - O * Math.sin(((R - r) / r) * t * speed);
+                    double lastX = (R - r) * Math.cos(lastUpdateTime * speed) + O * Math.cos(((R - r) / r) * lastUpdateTime * speed);
+                    double lastY = (R - r) * Math.sin(lastUpdateTime * speed) - O * Math.sin(((R - r) / r) * lastUpdateTime * speed);
+
+                    context.setStroke(Color.RED);
+                    context.strokeLine(lastX + centreX, lastY + centreY, x + centreX, y + centreY);
+
+                    lastUpdateTime = t;
+                }
+            }
+        };
     }
 
     @FXML
     protected void draw() {
         GraphicsContext context = canvas.getGraphicsContext2D();
-        float centreX = (float) (canvas.getWidth() * .5f);
-        float centreY = (float) (canvas.getHeight() * .5f);
         context.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        context.setStroke(Color.RED);
 
-        float t = 0;
-        for (int i = 0; i < 10000; ++i) {
-            float r = 134, R = 81, O = 99, t2 = t + timeStep * .001f;
-            float x = (float) ((R - r) * Math.cos(t) + O * Math.cos(((R - r) / r) * t));
-            float y = (float) ((R - r) * Math.sin(t) - O * Math.sin(((R - r) / r) * t));
-            float x2 = (float) ((R - r) * Math.cos(t2) + O * Math.cos(((R - r) / r) * t2));
-            float y2 = (float) ((R - r) * Math.sin(t2) - O * Math.sin(((R - r) / r) * t2));
-            context.strokeLine(x + centreX, y + centreY, x2 + centreX, y2 + centreY);
-            t = t2;
-        }
+        firstFrame = true;
+        lastUpdateTime = 0;
+        timer = 0;
+        animation.start();
     }
 }
