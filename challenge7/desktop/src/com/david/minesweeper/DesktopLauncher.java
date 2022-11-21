9415f014a9a8f48c862f5e4441f2f536b2cdd51a
David Crossman <davidocrossman@gmail.com>
2022-11-21 20:36:39 +0000

    Setup libGDX

---
+++ challenge7/desktop/src/com/david/minesweeper/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.david.minesweeper;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Minesweeper");
+		new Lwjgl3Application(new Minesweeper(), config);
+	}
+}
